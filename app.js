c3ffe3dcd4300804a84ce3d23472cbabd970db46
Yeabsera <yeabsera0830@gmail.com>
2022-12-04 16:06:08 +0300

    Some basic updates

--- app.js
+++ app.js
@@ -19,26 +19,31 @@ app.get("/:id", async (req, res) => {
 })
 
 app.post("/upload", storage.any(), async (req, res) => {
-  if (!req.files[0]) {
-    return res.status(400).send("Please send at least one file")
-  }
-
-  const arrayFiles = []
-
-  req.files.forEach(async file => {
-    fs.rename(`store/${file.filename}`, `store/${file.originalname}`, (err) => {
-      if (err) {
-        res.status(400).send("Error occured uploading the image")
-      } else {
-        files.push(appname + file.originalname)
-        arrayFiles.push(appname + file.originalname)
-      }
+  try {
+    if (!req.files[0]) {
+      return res.status(400).send("Please send at least one file")
+    }
+  
+    const arrayFiles = []
+    req.files.forEach(async file => {
+      fs.rename(`store/${file.filename}`, `store/${file.originalname}`, (err) => {
+        if (err) {
+          res.status(400).send("Error occured uploading the image")
+        } else {
+          files.push(appname + file.originalname)
+          arrayFiles.push(appname + file.originalname)
+        }
+      })
     })
-  })
-
-  const files = req.files.map(file => appname + file.originalname)
-  const response = files.length > 0? files[0] : "Image not uploaded"
-  res.send(response)
+  
+    const files = req.files.map(file => appname + file.originalname)
+    const response = files.length > 0? files[0] : "Image not uploaded"
+    res.send(response)
+  } catch(_) {
+    res.status(500).json({
+      message: "Internal Server Error"
+    })
+  }
 })
 
 app.listen(PORT, () => console.log(`Server listenting on *:${PORT}`))
